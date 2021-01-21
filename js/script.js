10df2c2070e01bf93f62db8adb4ee959e6fe0d74
max-i-mys <maximys6205@gmail.com>
2021-01-21 20:23:44 +0200

    create: processing commits

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-const testSlider = new Swiper('.partners__container',{
-	slidesPerView: 1,
-	spaceBetween: 10,
+const partnerSlider = new Swiper('.partners__container',{
+	slidesPerView: 5,
+	spaceBetween: 42,
 	loop: true,
 	autoplay: {
 		delay: 4000,
 		disableOnInteraction: false,
 	},
-	breakpoints: {
+	// breakpoints: {
 			// 768: {
 			// 		slidesPerView: 2,
 			// 		spaceBetween: 20
 			// },
-			992:{
-					slidesPerView: 5,
-					spaceBetween: 42
-			}
-	}
+	// 		992:{
+	// 				slidesPerView: 5,
+	// 				spaceBetween: 42
+	// 		}
+	// }
+})
+const commitsSlider = new Swiper('.sw-com-container',{
+	slidesPerView: 3,
+	spaceBetween: 50,
+	loop: true
 })
